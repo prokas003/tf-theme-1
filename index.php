9c6fb9c5253152af22d177a8943fd99f7ccbb2e0
prokas <prokas003@gmail.com>
2020-08-06 14:20:50 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="row">
+                <div class="logo col-md-2 col-sm-2">
+                    <img class="" src="images/logo/logo-dark-tfinternet.png" alt="Logo">
+                </div>
+            </div>
+
+        </div>
+    </header>
+
+
+
+
+    <footer class="footer text-center">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <h3>VitaeLab</h3>
+                <div class="logo col-md-12 col-sm-12">
+                    <img class="" src="images/logo/logo-dark-tfinternet.png" alt="Logo">
+                </div>
+                </div>
+                <div class="kontakt col-md-3 col-sm-6">
+                    <h3>Kontakt os</h3>
+                    <p>Adresse:
+                        NutraQ ApS, Livjægergade 17B, 2100 København Ø | CVR: 33 07 25 03</p>
+                        <p>Kontakt kundeservice:
+                            Telefon: 70 80 81 81 - Mandag til fredag 08.30-16.30
+                            Mail: kundeservice@vitaelab.dk  - (Svar inden for 2 hverdage)</p>
+
+                        </div>
+                        <div class="herkan col-md-3 col-sm-6">
+                            <h3>Her kan du læse om</h3>
+                            <p>- <a href="#"> Spørgsmål og svar - VitaePro</a></p>
+                            <p>- <a href="#"> Kundeservice </a></p>
+                            <p>- <a href="#"> Abonnements- og</a></p>
+                            <p> <a href="#"> købsbetingelser</a></p>
+                            <p>-  <a href="#"> Persondatapolitik</a></p>
+                            <p>-<a href="#"> Cookies brug</a></p>
+
+                        </div>
+                        <div class="col-md-3 col-sm-6">
+                            <h3>Følg os på Facebook</h3>
+                            <i class="fab fa-facebook"></i>
+                        </div>
+                    </div>
+                </div>
+            </footer>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        </body>
+        </html>
